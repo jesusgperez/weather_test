a2336edad2c4ad1df4bfda64cb1f13f77b734d8c
Jesus Perez <jesusgperez16@gmail.com>
2023-11-25 17:23:31 -0500

    Create the weather endpoint

--- weather-app/app.py
+++ weather-app/app.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask, request
 from flask_caching import Cache
+from config import *
 
 app = Flask(__name__)
-cache = Cache(app, config={'CACHE_TYPE': 'RedisCache', 'CACHE_REDIS_URL': 'rediss://127.0.0.1:6379'})
+cache = Cache(app, config=CACHE_CONFIG)
 
 @app.route('/')
 def main():
     return {'hello': 'world'}
+
+@app.route('/weather', methods=['GET', 'POST'])
+def weather_service():
+    if request.method == 'GET':
+        return {
+            'country': cache.get('country'),
+            'city': cache.get('city')
+        }
+
+    args = request.args
+
+    country, city = args.get('country'), args.get('city')
+
+    if not country or not city:
+        return 'incorrent args'
+
+    cache.set('country', country, timeout=10)
+    cache.set('city', city, timeout=10)
+
+    return 'this is a post request'
---
+++ weather-app/config.py
@@ -0,0 +1,4 @@
+CACHE_CONFIG = {
+    'CACHE_TYPE': 'RedisCache',
+    'CACHE_REDIS_URL': 'redis://127.0.0.1:6379'
+}
