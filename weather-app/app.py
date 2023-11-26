b820428aa7d6b667d872d97e42161c34c4230230
Jesus Perez <jesusgperez16@gmail.com>
2023-11-26 13:27:57 -0500

    Create test file and refactor the route

--- weather-app/app.py
+++ weather-app/app.py
@@ -1,10 +1,11 @@
-import json
-import requests
 from config import *
 from flask_caching import Cache
 from flask import Flask, request
 from response import bad_request, success_request
-from controller import serialize_data
+from controller import (
+    get_weather,
+    validate_arguments
+)
 
 app = Flask(__name__)
 cache = Cache(app, config=CACHE_CONFIG)
@@ -17,31 +18,11 @@ def main():
 
 @app.route('/weather', methods=['GET'])
 def weather_service():
-    args = request.args
-    country, city = args.get('country'), args.get('city')
+    valid, msg = validate_arguments(query_params=request.args)
 
-    if not country or not city:
-        msg = 'Should send a country and city to make a request'
+    if not valid:
         return bad_request(message=msg)
 
-    if not len(country) == 2:
-        msg = 'Country should have only two chars'
-        return bad_request(message=msg)
-
-    country, city = country.lower(), city.lower()
-
-    cached_response = cache.get(f'{country}_{city}')
-
-    if cached_response:
-        response = serialize_data(api_data=cached_response)
-        return success_request(response)
-
-    request_url = API_URL % (city, country)
-
-    api_response = requests.get(request_url)
-    api_data = json.loads(api_response.content)
-
-    cache.set(f'{country}_{city}', api_data)
-
-    response = serialize_data(api_data=api_data)
-    return success_request(message=response)
+    return success_request(
+        message=get_weather(query_params=request.args)
+    )
--- weather-app/controller.py
+++ weather-app/controller.py
@@ -1,6 +1,42 @@
-from typing import Dict
+import json
+import requests
+from app import cache, API_URL
+from typing import Dict, Tuple
 from domain import TemperatureType
 from datetime import datetime, timedelta
+
+
+def validate_arguments(query_params: Dict) -> Tuple[bool, str]:
+    country, city = query_params.get('country'), query_params.get('city')
+
+    if not country or not city:
+        msg = 'Should send a country and city to make a request'
+        return False, msg
+
+    if not len(country) == 2:
+        msg = 'Country should have only two chars'
+        return False, msg
+
+    return True, ''
+
+
+def get_weather(query_params: Dict) -> Dict:
+    country, city = query_params['country'], query_params['city']
+    country, city = country.lower(), city.lower()
+
+    cached_response = cache.get(f'{country}_{city}')
+
+    if cached_response:
+        return serialize_data(api_data=cached_response)
+
+    request_url = API_URL % (city, country)
+
+    api_response = requests.get(request_url)
+    api_data = json.loads(api_response.content)
+
+    cache.set(f'{country}_{city}', api_data)
+
+    return serialize_data(api_data=api_data)
 
 
 def serialize_data(api_data: Dict) -> Dict:
